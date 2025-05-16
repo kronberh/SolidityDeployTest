24ffc5022f643816bc804ddfe46d1343ab88a7eb
kronberh <10252213@stud.op.edu.ua>
2025-05-16 18:29:41 +0300

    updated deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,10 +2,14 @@ const {ethers} = require('./setup');
 
 async function main() {
     const Factory = await ethers.getContractFactory('Lesson2');
+    const Factory2 = await ethers.getContractFactory('Homework2');
+
     const lesson2 = await Factory.deploy();
+    const homework2 = await Factory2.deploy();
 
     await lesson2.waitForDeployment();
-    console.log('Contract deployed to ' + await lesson2.getAddress());
+    await homework2.waitForDeployment();
+    console.log('Contracts deployed to ' + await lesson2.getAddress() + ' and ' + await homework2.getAddress());
 }
 
 main().catch(error => {
